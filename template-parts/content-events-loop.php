177f9e0fda3ffd07c7218fa91aed0182ad10ec48
David <mezanddav@gmal.com>
2019-01-28 10:39:37 +0000

    list page

--- template-parts/content-events-loop.php
+++ template-parts/content-events-loop.php
@@ -11,9 +11,9 @@
 <div class="article__column">
 	<article id="post-<?php the_ID(); ?>" <?php post_class('article__event'); ?>>
 		<?php if( has_post_thumbnail( get_the_ID() ) ): ?>
-		<div class="article__img sh" data-img-type="post-thumb">
+		<a class="article__img sh" href="<?php echo get_permalink(); ?>" data-img-type="post-thumb">
 			<img class="evenp__banner-img loadlzly" src="<?php echo get_the_post_thumbnail_url( get_the_ID(), 'two-one-events-thumb' ); ?>" data-src="<?php echo get_the_post_thumbnail_url( get_the_ID(), 'two-one-events-full' ); ?>">
-		</div>
+		</a>
 		<?php endif; ?>
 		<a class="article__event-uri" href="<?php echo get_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title( '<h2 class="article__event-title">', '</h2>' ); ?></a>
 		<div class="article__presenter"><?php eve_get_profile( 'presenter', get_the_ID() ); ?></div>
