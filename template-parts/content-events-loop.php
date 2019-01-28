eeb993f784eb45d3cf561f10f7d6be35c4b153c4
David <mezanddav@gmal.com>
2019-01-28 01:41:59 +0000

    Event post page

--- template-parts/content-events-loop.php
+++ template-parts/content-events-loop.php
@@ -9,14 +9,14 @@
 
 ?>
 <div class="article__column">
-	<article id="post-<?php the_ID(); ?>" <?php post_class('article__events'); ?>>
+	<article id="post-<?php the_ID(); ?>" <?php post_class('article__event'); ?>>
 		<!-- <div class="article__img">kép</div> -->
-		<?php the_title( '<h2 class="article__event-title">', '</h2>' ); ?>
+		<a class="article__event-uri" href="<?php echo get_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title( '<h2 class="article__event-title">', '</h2>' ); ?></a>
 		<div class="article__presenter"><?php eve_get_profile( 'presenter', get_the_ID() ); ?></div>
 		<div class="article__details">
 			<div class="article__details-left"></div>
 			<div class="article__details-right"></div>
 		</div>
-		<div class="article__cta"><a class="btn btn-light small block" href="<?php echo get_permalink(); ?>"><?php _e('BŐVEBBEN', 'eve'); ?></a></div>
+		<div class="article__cta"><a class="btn btn-light small block" href="<?php echo get_permalink(); ?>" title="<?php the_title(); ?>"><?php _e('BŐVEBBEN', 'eve'); ?></a></div>
 	</article>
 </div>
