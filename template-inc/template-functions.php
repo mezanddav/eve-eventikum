e37ae7be784ca84426a7f7a5cd4831e825a4ab7e
David <mezanddav@gmal.com>
2019-02-06 20:41:38 +0000

    Lofasz

--- template-inc/template-functions.php
+++ template-inc/template-functions.php
@@ -436,6 +436,22 @@ if ( !function_exists( 'progresseve_scripts' ) ) :
 
     // map.panBy(0, -80);  -  Line 491
 
+    /**
+     * (function() {
+     *   var container = document.getElementById( 'get-your-city' );
+     *   if ( !container ) { return; };
+     *   var user_ip = '<?php echo $user_ip; ?>';
+     *   var api_url = 'http://ip-api.com/json/';
+     *   var api = api_url + user_ip;
+     *   var request = $.getJSON(api).done(function(data){
+     *     if( data.status == 'success' ){
+     *       $(container).val(data.city);
+     *       debug['location'] = data.city;
+     *     }
+     *   })
+     * })();
+     */
+
 ?><script type='text/javascript'>
 var debug = [];
 !function(){var n,e,a,t,s,i;if((n=document.getElementById("site-navigation"))&&void 0!==(e=n.getElementsByTagName("button")[0]))if(void 0!==(a=n.getElementsByTagName("ul")[0])){for(a.setAttribute("aria-expanded","false"),-1===a.className.indexOf("nav-menu")&&(a.className+=" nav-menu"),e.onclick=function(){-1!==n.className.indexOf("toggled")?(n.className=n.className.replace(" toggled",""),e.setAttribute("aria-expanded","false"),a.setAttribute("aria-expanded","false")):(n.className+=" toggled",e.setAttribute("aria-expanded","true"),a.setAttribute("aria-expanded","true"))},s=0,i=(t=a.getElementsByTagName("a")).length;s<i;s++)t[s].addEventListener("focus",l,!0),t[s].addEventListener("blur",l,!0);!function(e){var a,t,s=n.querySelectorAll(".menu-item-has-children > a, .page_item_has_children > a");if("ontouchstart"in window)for(a=function(e){var a,t=this.parentNode;if(t.classList.contains("focus"))t.classList.remove("focus");else{for(e.preventDefault(),a=0;a<t.parentNode.children.length;++a)t!==t.parentNode.children[a]&&t.parentNode.children[a].classList.remove("focus");t.classList.add("focus")}},t=0;t<s.length;++t)s[t].addEventListener("touchstart",a,!1)}()}else e.style.display="none";function l(){for(var e=this;-1===e.className.indexOf("nav-menu");)"li"===e.tagName.toLowerCase()&&(-1!==e.className.indexOf("focus")?e.className=e.className.replace(" focus",""):e.className+=" focus"),e=e.parentElement}}();
@@ -547,20 +563,6 @@ jQuery(document).ready(function($) {
       $($(this).data('fade')).hide();
       $($(this).data('ctn')).css('height', 'auto');
 	  });
-  })();
-
-  (function() {
-    var container = document.getElementById( 'get-your-city' );
-		if ( !container ) { return; };
-    var user_ip = '<?php echo $user_ip; ?>';
-    var api_url = 'http://ip-api.com/json/';
-    var api = api_url + user_ip;
-    var request = $.getJSON(api).done(function(data){
-      if( data.status == 'success' ){
-        $(container).val(data.city);
-        debug['location'] = data.city;
-      }
-    })
   })();
 });
 </script>
